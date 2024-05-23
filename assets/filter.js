75508053f8f33de22f954b63989fd94c0335d012
Dylan Gilbert <39006574@parisnanterre.fr>
2024-05-23 18:50:26 +0200

    filter gallery

--- assets/filter.js
+++ assets/filter.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const galleryItems = document.querySelectorAll(".gallery-item");
 
+    galleryItems.forEach(function(item) {
+        item.parentNode.style.display = "block";
+    });
+
     document.querySelectorAll(".tag-filter").forEach(function(filter) {
         filter.addEventListener("click", function() {
             const tag = this.dataset.tag;
             galleryItems.forEach(function(item) {
                 if (tag === "all" || item.dataset.galleryTag === tag) {
-                    item.style.display = "block";
+                    item.parentNode.style.display = "block";
                 } else {
-                    item.style.display = "none";
+                    item.parentNode.style.display = "none";
                 }
             });
         });
